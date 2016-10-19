0d242a9f1da494d545b4acef6fa17788605c8982
Bill Peregoy <bill@billperegoy.com>
2016-10-19 18:40:20 -0400

    Added support for elm-live and elm-test.

--- src/Main.elm
+++ src/Main.elm
@@ -1,39 +1,63 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
+import Utils
+
 
 main : Program Never
 main =
-  Html.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
-type alias Model = 
-  { name : String }
 
-init : (Model, Cmd Msg)
+-- Model
+
+
+type alias Model =
+    { name : String }
+
+
+init : ( Model, Cmd Msg )
 init =
-  (Model "world", Cmd.none)
+    Model "world" ! []
 
-type Msg 
-  = NoOp
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+-- Update
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    NoOp ->
-      (model, Cmd.none)
+    case msg of
+        NoOp ->
+            model ! []
 
-view : Model ->  Html Msg
+
+
+-- View
+
+
+view : Model -> Html Msg
 view model =
-  div []
-    [ text ("Hello " ++ model.name)]
+    h1 []
+        [ text ("Hello " ++ (Utils.capitalize model.name)) ]
+
+
+
+-- Subscriptions
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
